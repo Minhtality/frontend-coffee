dca3a1325bc1faa95b69b26c76db5fe1bac62426
Minh <mtran1712@gmail.com>
2024-04-16 22:39:59 -0700

    temporarily move out coffee page

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,40 +1,46 @@
+import * as Styled from "./index.styled";
 import Link from "next/link";
 import React from "react";
-import { FiShoppingBag } from "react-icons/fi";
-import * as Styled from "./index.styled";
-import { useProductContext } from "@/context";
 import Cart from "../Cart";
 import User from "../User";
+import { FiShoppingBag } from "react-icons/fi";
+import { useProductContext } from "@/context";
 import { AnimatePresence, motion } from "framer-motion";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { usePathname } from "next/navigation";
 
 export default function Nav() {
   const { cartItems, setShowCart, showCart } = useProductContext();
-  const { user, error, isLoading } = useUser();
+  const pathname = usePathname();
 
   const totalQuantity = cartItems.reduce((acc, curr) => {
     return acc + curr.quantity;
   }, 0);
 
+  const returnHref = () => {
+    if (pathname === "/coffee" || pathname.startsWith("/product")) {
+      return "/coffee";
+    } else {
+      return "/";
+    }
+  };
+
   return (
     <Styled.NavWrapper>
-      <Link href="/">Good Soup.</Link>
-      <Styled.NavItems>
-        <Styled.Item onClick={() => setShowCart(true)}>
-          <FiShoppingBag />
-          <h3>Cart</h3>
-          {cartItems.length > 0 && (
-            <motion.span animate={{ scale: 1 }} initial={{ scale: 0 }}>
-              {totalQuantity}
-            </motion.span>
-          )}
-        </Styled.Item>
-        <User />
-        {/* <div>
-          <FiShoppingBag />
-          <h3>Cart</h3>
-        </div> */}
-      </Styled.NavItems>
+      <Link href={returnHref()}>Good Soup.</Link>
+      {(pathname === "/coffee" || pathname.startsWith("/product")) && (
+        <Styled.NavItems>
+          <Styled.Item onClick={() => setShowCart(true)}>
+            <FiShoppingBag />
+            <h3>Cart</h3>
+            {cartItems.length > 0 && (
+              <motion.span animate={{ scale: 1 }} initial={{ scale: 0 }}>
+                {totalQuantity}
+              </motion.span>
+            )}
+          </Styled.Item>
+          <User />
+        </Styled.NavItems>
+      )}
       <AnimatePresence>
         {showCart && <Cart cartItems={cartItems} />}
       </AnimatePresence>
